a8bbb2e5f15b1c9e36ea8dcd8bf2c3dfcbcc14a6
flabbergast <s3+flabbergast@sdfeu.org>
2014-12-04 15:40:18 +0000

    Move power_up from SHA204 lib to the main code.

--- avr/SHA204/SHA204SWI_hardware_config.h
+++ avr/SHA204/SHA204SWI_hardware_config.h
@@ -15,20 +15,10 @@
 /*************************\
  **** FOR XMEGA CHIPS ****
 \*************************/
-#if (defined(__AVR_ATxmega128A3U__))
+#if defined(__AVR_ATxmega128A3U__) || defined(__AVR_ATxmega128A4U__)
   // Modify below!
-  //  -> If you use actual GND and VCC pins for power, ignore the _VCC_ and _GND_ settings
-  //     and redefine the S_POWER_UP macro to be blank
   #define SHA204_PORT PORTB
   #define SHA204_BIT (1 << 2)
-  #define SHA204_GND_PORT PORTB
-  #define SHA204_GND_BIT (1 << 3)
-  #define SHA204_VCC_PORT PORTB
-  #define SHA204_VCC_BIT (1 << 1)
-
-  // Choose the appropriate S_POWER_UP macro according to your power setup
-  // #define S_POWER_UP {}  // use this if using actual GND and VCC supply
-  #define S_POWER_UP SHA204_GND_PORT.DIRSET = SHA204_GND_BIT; SHA204_GND_PORT.OUTCLR = SHA204_GND_BIT; SHA204_VCC_PORT.DIRSET = SHA204_VCC_BIT; SHA204_VCC_PORT.OUTSET = SHA204_VCC_BIT // use this if powering from PORT pins
 
   // Leave the following code alone
   #define S_PIN_DIR_OUT SHA204_PORT.DIRSET = SHA204_BIT
@@ -42,8 +32,6 @@
 \********************************/
 #else
   // Modify below!
-  //  -> If you use actual GND and VCC pins for power, ignore the _VCC_ and _GND_ settings
-  //     and redefine the S_POWER_UP macro to be blank
   //  -> Have a look at http://arduino.cc/en/Hacking/PinMapping168 to find out
   //     the correct port/bit from an Arduino pin number (just need to change the
   //     last letters and the bit, e.g. Arduino's digital pin 7 is "PD7", so one
@@ -52,16 +40,6 @@
   #define SHA204_PIN PINB
   #define SHA204_DDR DDRB
   #define SHA204_BIT (1<<1)
-  #define SHA204_GND_PORT PORTB
-  #define SHA204_GND_DDR DDRB
-  #define SHA204_GND_BIT (1 << 0)
-  #define SHA204_VCC_PORT PORTB
-  #define SHA204_VCC_DDR DDRB
-  #define SHA204_VCC_BIT (1 << 2)
-
-  // Choose the appropriate S_POWER_UP macro according to your power setup
-  #define S_POWER_UP {}  // use this if using actual GND and VCC supply
-  // #define S_POWER_UP SHA204_GND_DDR|=SHA204_GND_BIT; SHA204_GND_PORT&=~SHA204_GND_BIT; SHA204_VCC_DDR|=SHA204_VCC_BIT; SHA204_VCC_PORT|=SHA204_VCC_BIT;  // use this if powering from PORT pins
 
   // Leave the following code alone
   #define S_PIN_DIR_OUT SHA204_DDR |= SHA204_BIT
